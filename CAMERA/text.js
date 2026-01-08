0ad79685f9ccae52f3397a7f4383863d650afbb5
josh-king1 <akawejoshua2020@gmail.com>
2026-01-08 21:58:47 +0100

    Add initial HTML and JavaScript files for camera feed functionality

---
+++ CAMERA/text.js
@@ -0,0 +1,9 @@
+const cameraFeed = document.getElementById('cameraFeed');
+
+navigator.mediaDevices.getUserMedia({ video: true })
+.then(stream => {
+    cameraFeed.srcObject = stream;
+})
+.catch(err => {
+    console.error("Error accessing camera: ", err);
+});
